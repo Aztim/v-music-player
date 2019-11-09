d107ef4135d9d5320c4c9624042ac08ecff5290b
Tim-82 <barboza-82@mail.ru>
2019-11-09 23:15:24 +0300

    generate a new app

---
+++ src/components/Player.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-system-bar window>
+    <v-menu offset-y transition="slide-y-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn text small right   v-on="on"> <!--right-->
+          <v-icon>mdi-headset</v-icon>MENU
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item  @click="dialog = true">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <v-dialog v-model="dialog" max-width="300">
+            <v-card>
+              <v-card-title><h2>Vue Music Player</h2></v-card-title>
+                <v-card-text>Version 3.0.0</v-card-text>        
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn text @click="dialog = false">OK</v-btn>
+              </v-card-actions>
+            </v-card>
+        </v-dialog>
+      </v-list>
+    </v-menu>
+    <v-spacer></v-spacer>
+    VUE MUSIC PLAYER
+    <v-spacer></v-spacer>
+    <v-icon>mdi-minus-circle-outline</v-icon>
+    <v-icon>mdi-checkbox-blank-outline</v-icon>
+    <v-icon>mdi-close-circle</v-icon>
+  </v-system-bar>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  data () {
+    return {
+      dialog: false,
+    }
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+// import Player from './Player.vue'
+import vuetify from './plugins/vuetify';
 import App from './App.vue'
+// import {Howl, Howler} from 'howler';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
