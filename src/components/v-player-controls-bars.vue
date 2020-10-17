9c0933d8d52e2545bcaf75681b3004be6564f243
Tim-82 <barboza-82@mail.ru>
2020-10-17 23:30:50 +0300

    delete vuetify design in control-bars component

--- src/components/v-player-controls-bars.vue
+++ src/components/v-player-controls-bars.vue
@@ -20,50 +20,9 @@
     </div>
     {{ this.volume * 100 + '%' }}
     <div id="seekbar">
-      <input type="range" oninput="setPos(this.value)" id="seek" value="0" max="">
+      <input type="range" :value="trackProgress" oninput="setPos(this.value)" id="seek">
     </div>
   </div>
-    <!-- <v-toolbar text height=90>
-      <v-btn text icon @click="toggleMute">
-        <template v-if="!this.muted">
-          <v-icon v-if="this.volume >= 0.5">mdi-volume-high</v-icon>
-          <v-icon v-else-if="this.volume > 0">mdi-volume-medium</v-icon>
-          <v-icon v-else>mdi-volume-variant-off</v-icon>
-        </template>
-        <v-icon v-show="this.muted">mdi-volume-off</v-icon>
-      </v-btn>
-      <v-slider v-model="volume" @input="updateVolume(volume)" max="1" step="0.1"></v-slider>
-      {{ this.volume * 100 + '%' }}
-      <v-spacer></v-spacer>
-      <v-btn outlined fab small color="light-blue" @click="skipTrack('prev')">
-        <v-icon>mdi-skip-previous</v-icon>
-      </v-btn>
-      <v-btn outlined fab small color="light-blue" @click="stopTrack">
-        <v-icon>mdi-stop</v-icon>
-      </v-btn>
-      <v-btn outlined fab color="light-blue" @click="playTrack()">
-        <v-icon large>mdi-play</v-icon>
-      </v-btn>
-      <v-btn outlined fab small color="light-blue" @click="pauseTrack">
-        <v-icon>mdi-pause</v-icon>
-      </v-btn>
-      <v-btn outlined fab small color="light-blue" @click="skipTrack('next')">
-        <v-icon>mdi-skip-next</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn text icon @click="toggleLoop">
-        <v-icon color="light-blue" v-if="this.loop">mdi-repeat-once</v-icon>
-        <v-icon color="blue-grey" v-else>mdi-repeat-once</v-icon>
-      </v-btn>
-      <v-btn text icon @click="toggleShuffle">
-        <v-icon color="light-blue" v-if="this.shuffle">mdi-shuffle</v-icon>
-        <v-icon color="blue-grey" v-else>mdi-shuffle</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-toolbar text height="40">
-      <v-progress-linear height="40" :value="trackProgress" @click="updateSeek($event)" class="progress-linear__bar"></v-progress-linear>
-    </v-toolbar>
-  </div> -->
 </template>
 
 <script>
@@ -108,12 +67,12 @@ export default {
       Howler.mute(!this.muted)
       this.muted = !this.muted
     },
-    toggleLoop () {
-      this.$emit('toggleloop', !this.loop)
-    },
-    toggleShuffle () {
-      this.$emit('toggleshuffle', !this.shuffle)
-    },
+    // toggleLoop () {
+    //   this.$emit('toggleloop', !this.loop)
+    // },
+    // toggleShuffle () {
+    //   this.$emit('toggleshuffle', !this.shuffle)
+    // },
     updateSeek (event) {
       const el = document.querySelector('.progress-linear__bar')
       const mousePos = event.offsetX
