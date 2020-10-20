f79d64aacc4aed3df38aac9e980b24a57dd906dd
Tim-82 <barboza-82@mail.ru>
2020-10-20 15:58:05 +0300

    change player-controls-bars component

--- src/components/v-player-controls-bars.vue
+++ src/components/v-player-controls-bars.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <div class="playback_controls">
-      <button @click="skipTrack('prev')"><i class="fa fa-fast-backward"></i></button>
-      <button @click="pauseTrack"><i class="fa fa-pause"></i></button>
-      <button @click="playTrack()"><i class="fa fa-play"></i></button>
-      <button @click="stopTrack"><i class="fa fa-stop"></i></button>
-      <button @click="skipTrack('next')"><i class="fa fa-fast-forward"></i></button>
-    </div>
     <div class="volume_controls">
       <button id="mute"  @click="toggleMute">
         <div v-if="!this.muted">
@@ -17,9 +10,17 @@
         <i v-show="this.muted" class="fa fa-volume-mute"></i>
       </button>
       <input type="range" id="volume" v-model="volume" @input="updateVolume(volume)" max="1" step="0.1">
+      {{ this.volume * 100 + '%' }}
     </div>
-    {{ this.volume * 100 + '%' }}
-    <div id="seekbar">
+
+    <div class="controls">
+      <button @click="skipTrack('prev')"><i class="fa fa-fast-backward"></i></button>
+      <button @click="pauseTrack"><i class="fa fa-pause"></i></button>
+      <button @click="playTrack()"><i class="fa fa-play"></i></button>
+      <button @click="stopTrack"><i class="fa fa-stop"></i></button>
+      <button @click="skipTrack('next')"><i class="fa fa-fast-forward"></i></button>
+    </div>
+    <div class="seekbar">
       <input type="range" :value="trackProgress" @click="updateSeek($event)" id="seek">
     </div>
   </div>
